4bd9f6a92776aed7ee101214ef29dc48250acb8a
monnef <mkucera22@gmail.com>
2020-01-23 18:18:21 +0100

    Update dependencies
    Fix network adapter to compile in new TS
    Added IDEA related things to gitignore

--- src/adapters/Network.ts
+++ src/adapters/Network.ts
@@ -54,7 +54,7 @@ export default class Network extends Adapter {
     public async write(data: Uint8Array): Promise<void> {
         return new Promise<void>(resolve => {
             this.throwIfNeeded();
-            this.device.write(new Buffer(data), resolve);
+            this.device.write(new Buffer(data), () => resolve());
         });
     }
 
